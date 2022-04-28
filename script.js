b2e2a764475357f81d98906a89ef5fbbaad6a2a3
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-04-28 16:26:25 +0200

    modify small details on main

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ function SlideShow() {
   slidePosition++;
   if (slidePosition > slides.length) {slidePosition = 1}
   slides[slidePosition-1].style.display = "block";
-  setTimeout(SlideShow, 2000); // Change image every 2 seconds
+  setTimeout(SlideShow, 3000); // Change image every 2 seconds
 }
