602b041c125af396a1d8ae7acce92902666dab21
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-04-28 16:08:47 +0200

    fix bug about carrousel on script.js

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
 /* ---------------    carrousel    ---------------*/
-var slidePosition = 1;
-SlideShow(slidePosition);
+var slidePosition = 0;
+SlideShow();
 
-// forward/Back controls
-function plusSlides(n) {
-  SlideShow(slidePosition += n);
-}
-
-//  images controls
-function currentSlide(n) {
-  SlideShow(slidePosition = n);
-}
-
-function SlideShow(n) {
+function SlideShow() {
   var i;
   var slides = document.getElementsByClassName("Containers");
-  var circles = document.getElementsByClassName("dots");
-  if (n > slides.length) {slidePosition = 1}
-  if (n < 1) {slidePosition = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < circles.length; i++) {
-      circles[i].className = circles[i].className.replace(" enable", "");
-  }
+  slidePosition++;
+  if (slidePosition > slides.length) {slidePosition = 1}
   slides[slidePosition-1].style.display = "block";
-  circles[slidePosition-1].className += " enable";
+  setTimeout(SlideShow, 2000); // Change image every 2 seconds
 }
-
