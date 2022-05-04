0695a4405280d079765041aa2a1c4f96d4b4de39
LANCELLE Xavier (SNCF VOYAGEURS / DIRECTION DE LIGNES ALPES / Siège UO Servi CHAMBERY) <xavilanc@gmail.com>
2022-05-04 19:42:29 +0200

    add menu burger

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function SlideShow() {
   slides[slidePosition-1].style.display = "block";
   setTimeout(SlideShow, 3000); // Change image every 2 seconds
 }
+
+/* ---------------    menu burger    ---------------*/
+
+let sidenav = document.getElementById("mySidenav");
+let openBtn = document.getElementById("openBtn");
+let closeBtn = document.getElementById("closeBtn");
+
+openBtn.onclick = openNav;
+closeBtn.onclick = closeNav;
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  sidenav.classList.add("active");
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  sidenav.classList.remove("active");
+}
